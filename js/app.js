07683879590fa5f9db6479f56a1aac5302d9b7f4
rkota <48548004+rosairekota@users.noreply.github.com>
2021-04-12 15:29:42 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", getJsonDatas);
 
 // peremet de recuperer le donnees en Ajax:
 function getJsonDatas() {
-  fetch(" http://localhost:3000/developper")
+  fetch(" https://my-json-server.typicode.com/rosaire-kota-kda-2021.github.io/portefolio-Ajax/developper")
     .then(function (response) {
       return response.json();
     })
